149e7061f01c8691beeaf5a7750539dd7256e362
dieegomp11 <dmp2003.11@gmail.com>
2026-05-12 15:04:45 +0200

    Switch to GitHub Pages static export

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/Alex-Rubio',
+  assetPrefix: '/Alex-Rubio',
   images: {
     unoptimized: true,
   },
